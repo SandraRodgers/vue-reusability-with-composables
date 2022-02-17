a9fd09fcc42f09ac9bc46ddd48743194b4a61560
Sandra Rodgers <sandra.rodgers@deepgram.com>
2022-02-17 12:38:36 -0600

    adding final refactor

---
+++ src/components/Mast.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="mast">
+    <div class="container">
+      <div class="image-container">
+        <img ref="imageRef" src="https://source.unsplash.com/Mv9hjnEUHR4" />
+      </div>
+      <div ref="textRef" class="text-container">
+        <p>
+          Meatball, 9. Barks at Amazon guy. Likes sharing your apple slices.
+          Wants you to grab the toy but won't let you have it.
+        </p>
+      </div>
+    </div>
+  </div>
+  Width:{{ elementWidth ? elementWidth : initialWidth }}
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+import useResizeText from "../composables/useResizeText";
+export default {
+  name: "Mast",
+  setup() {
+    let initialWidth = ref(0);
+
+    //template refs
+    let imageRef = ref(null);
+    let textRef = ref(null);
+
+    const breakPoints = { small: "100", medium: "150", large: "200" };
+    const textStyles = {
+      fontSize: { small: ".8em", medium: "1em", large: "1.3em" },
+      lineHeight: { small: "1.3", medium: "1.4", large: "1.5" },
+    };
+
+    onMounted(() => {
+      //send in refs and constants
+      const { elementWidth } = useResizeText(
+        imageRef,
+        textRef,
+        breakPoints,
+        textStyles
+      );
+      //get initial width for template
+      initialWidth.value = elementWidth.value;
+    });
+
+    //destructure to get data sent back from the composable
+    //get updated width for template
+    const { elementWidth } = useResizeText(
+      imageRef,
+      textRef,
+      breakPoints,
+      textStyles
+    );
+
+    return { imageRef, textRef, elementWidth, initialWidth };
+  },
+};
+</script>
+
+<style scoped>
+.mast {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  width: 60vw;
+}
+.image-container {
+  display: flex;
+  justify-content: center;
+  width: 60%;
+}
+
+.text-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Helvetica Neue";
+  font-size: 1.3em;
+  line-height: 1.5;
+  width: 40%;
+}
+
+img {
+  width: 50%;
+}
+</style>
---
+++ src/composables/useResizeText.js
@@ -0,0 +1,59 @@
+import { ref, watch, onUnmounted } from "vue";
+import useWindowEvent from "./useWindowEvent";
+
+export default function useResizeText(
+  triggerElement,
+  reactElement,
+  breakpoints,
+  styles
+) {
+  const { fontSize, lineHeight } = styles;
+  const { medium, large } = breakpoints;
+
+  let elementWidth = ref(0);
+
+  function resizeHandler() {
+    //tracking of width changes
+    if (triggerElement.value) {
+      elementWidth.value = triggerElement.value.offsetWidth;
+    }
+  }
+
+  //set initial value
+  if (triggerElement.value) {
+    elementWidth.value = triggerElement.value.offsetWidth;
+    setTextStyles();
+  }
+
+  //function to set text styles on mount and in watcher
+  function setTextStyles() {
+    if (elementWidth.value < medium) {
+      //small
+      reactElement.value.style.fontSize = fontSize.small;
+      reactElement.value.style.lineHeight = lineHeight.small;
+    }
+    if (elementWidth.value < large && elementWidth.value > medium) {
+      //medium
+      reactElement.value.style.fontSize = fontSize.medium;
+      reactElement.value.style.lineHeight = lineHeight.medium;
+    }
+    if (elementWidth.value > large) {
+      //large
+      reactElement.value.style.fontSize = fontSize.large;
+      reactElement.value.style.lineHeight = lineHeight.large;
+    }
+  }
+
+  //add and destroy event listeners
+  useWindowEvent("resize", resizeHandler, "add");
+  onUnmounted(() => {
+    useWindowEvent("resize", resizeHandler, "destroy");
+  });
+
+  //watch elementWidth and set text styles
+  watch(elementWidth, () => {
+    setTextStyles();
+  });
+
+  return { elementWidth };
+}
